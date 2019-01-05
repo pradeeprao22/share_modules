306572ae2161b6293ea93bde74edc8545282a46f
pradeeprao22 <pradeepraorayapudi@gmail.com>
2019-01-05 21:28:10 +0530

    Front-Back End Changes For Posts Views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
+  has_many :posts, dependent: :destroy
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :name, presence: true, length: {maximum: 50}       
+  validates :name, presence: true, length: {maximum: 50}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
   resources :users, only: [:show]
+
+  resources :posts, only: [:index, :show, :create] do
+     resources :photos, only: [:create]
+  end
 end
