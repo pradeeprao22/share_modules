fe67500783a902d7f0c3b19d4f2a11d7362e5b26
pradeeprao22 <pradeepraorayapudi@gmail.com>
2019-01-07 20:58:30 +0530

    Debug

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -19,7 +19,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
   process :tags => ['post_picture']
 
   version :standard do
-    process :resize_to_fill => [100, 150, :north]
+    process :resize_to_fill => [300, 300, :north]
   end
 
   version :thumbnail do
