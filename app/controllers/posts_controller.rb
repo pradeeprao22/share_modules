6ca38810cee8e9a6be2e29de98a3d0b720e6125a
pradeeprao22 <pradeepraorayapudi@gmail.com>
2019-01-19 12:45:22 +0530

    bookmarks update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :find_post, only: [:show, :destroy]
 
   def index
-    @posts = Post.all.limit(100).includes(:photos, :user, :likes).order('created_at desc')
+    @posts = Post.all.limit(100).includes(:photos, :user, :likes, :bookmarks).order('created_at desc')
     @post = Post.new
   end
 
