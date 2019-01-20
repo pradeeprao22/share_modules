5a871546035c2607ef9181e5b49eca9869162a6f
pradeeprao22 <pradeepraorayapudi@gmail.com>
2019-01-20 19:52:12 +0530

    new features

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
   validates :name, presence: true, length: {maximum: 50}
+
+  def self.search(term)
+     if term
+      where('name LIKE ?', "%#{term}%")
+     else
+       nil
+     end
+  end
 end
