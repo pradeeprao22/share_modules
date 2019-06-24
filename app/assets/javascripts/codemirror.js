bc95a95bcbf69b3d65406dc68c0c5080aae60535
Pradeep Rao <pradeep.rayapudi93@gmail.com>
2019-06-24 12:37:12 +0530

    added backend editor to show page, footer

--- app/assets/javascripts/codemirror.js
+++ app/assets/javascripts/codemirror.js
@@ -16,3 +16,12 @@ $(document).ready(function(){
         tabMode: "indent"
     });
 });
+
+$(document).ready(function(){
+    var code = $(".code-backend")[0];
+    var editor = CodeMirror.fromTextArea(code, {
+        lineNumbers : true,
+        mode: "css",
+        tabMode: "indent"
+    });
+});
