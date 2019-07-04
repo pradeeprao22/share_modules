3289052c09c55111313c2a79141afe264ecadf87
Pradeep Rao <pradeep.rayapudi93@gmail.com>
2019-07-04 18:28:41 +0530

    some changes in style and notifications

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,3 +19,19 @@
 //= require_tree .
 //= require toastr
 //= require codemirror
+
+/*global toastr*/
+toastr.options = {
+    "closeButton": false,
+    "debug": false,
+    "positionClass": "toast-bottom-right",
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+  }
