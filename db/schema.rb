0add0829e4238f06ec995efe54ced63b69e2c592
Pradeep Rao <pradeeprao@Pradeeps-MacBook-Pro.local>
2021-06-07 23:06:01 +0100

    feat: changes for deployment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_02_105530) do
+ActiveRecord::Schema.define(version: 2020_05_21_160119) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,6 +77,8 @@ ActiveRecord::Schema.define(version: 2019_07_02_105530) do
     t.string "instruction"
     t.text "front"
     t.text "frontend_css"
+    t.text "javascript"
+    t.integer "status"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
