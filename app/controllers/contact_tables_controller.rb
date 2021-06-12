3ec13611eecd4bddf72abfefb98a8bbbc2b90a6f
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2021-06-12 02:36:50 +0100

    fix: minor changes
    issue: 10005

--- app/controllers/contact_tables_controller.rb
+++ app/controllers/contact_tables_controller.rb
@@ -1,7 +1,11 @@
 class ContactTablesController < ApplicationController
     def create
       @contact_table = ContactTable.new(contact_table_params)
-      @contact_table.save
+      if @contact_table.save
+        flash[:alert] = "You details has been submited succesfully we will contact you in 2hrs...."
+      else 
+        flash[:alert] = "Something went wrong we are working on it...."
+      end
     end
   
     private
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -15,8 +15,12 @@ class PagesController < ApplicationController
   end
 
   def contact_table
-      @contact_table = ContactTable.new(contact_table_params)
-      @contact_table.save
+    @contact_table = ContactTable.new(contact_table_params)
+    if @contact_table.save
+      flash[:alert] = "You details has been submited succesfully we will contact you in 2hrs...."
+    else 
+      flash[:alert] = "Something went wrong we are working on it...."
+    end  
   end
 
   def innovation
