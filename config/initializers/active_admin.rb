c78196f2187fc5f7d4d61223864b10c203191956
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2021-06-19 22:24:00 +0100

    feat: fix activ admin route
    issue:10025

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -332,4 +332,17 @@ ActiveAdmin.setup do |config|
   # You can switch to using Webpacker here.
   #
   # config.use_webpacker = true
+
+
+  # == Friendly Id addon
+  ActiveAdmin::ResourceController.class_eval do
+    def find_resource
+      if resource_class.is_a?(FriendlyId)
+        scoped_collection.friendly.find(params[:id])
+      else
+        scoped_collection.find(params[:id])
+      end
+    end
+  end
+  # initial config
 end
