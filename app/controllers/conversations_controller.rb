8b913f81970a0a0e7932ee12d830616ea95d239b
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2021-07-07 13:04:17 +0100

    fix: content change
    issue: 10070

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -5,6 +5,7 @@ class ConversationsController < ApplicationController
     @users = User.all
     @conversations = Conversation.all
   end
+  
   def create  
     if Conversation.between(params[:sender_id], params[:recipient_id]).present? 
        @conversation = Conversation.between(params[:sender_id], params[:recipient_id]).first
