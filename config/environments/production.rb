c7914a9aa14e31d971fcda252239a0ad2ade6ba1
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2021-07-19 10:23:22 +0100

    fix: email fix on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
 
-  config.action_mailer.default_url_options = { :host => 'londevs.com' }
+  config.action_mailer.default_url_options = { :host => 'ai.londevs.com' }
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
