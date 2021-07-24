aa702355cb1dd29f565fc03a5763da468f1771b4
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2021-07-24 10:48:38 +0100

    feat: add action cable js
    issue: 10090

--- app/assets/javascripts/cable.js
+++ app/assets/javascripts/cable.js
@@ -11,3 +11,55 @@
   App.cable = ActionCable.createConsumer();
 
 }).call(this);
+
+$(document).on('turbolinks:load', function() {
+  if (App.posts) {
+    return App.posts.followVisibleprojects();
+  }
+});
+
+App.posts = App.cable.subscriptions.create("postsChannel", {
+  collection: function() {
+    return $('.posts-box');
+  },
+  connected: function() {
+    return setTimeout((function(_this) {
+      return function() {
+        return _this.followVisibleprojects();
+      };
+    })(this), 1000);
+  },
+  followVisibleposts: function() {
+    var i, len, results, post, posts;
+    posts = this.collection().map(function() {
+      return $(this).attr('data-post');
+    }).get();
+    if (posts.length > 0) {
+      results = [];
+      for (i = 0, len = posts.length; i < len; i++) {
+        post = posts[i];
+        results.push(this.perform('follow', {
+          post_id: post
+        }));
+      }
+      return results;
+    } else {
+      return this.perform('unfollow');
+    }
+  },
+  disconnected: function() {
+    return this.perform('unfollow');
+  },
+  received: function(data) {
+    var box;
+
+    console.log("[ActionCable] [post] [" + data.id + "]", data);
+    
+    box = $(".post-box[data-post='" + data.id + "']");
+    
+    if (box) {
+      return box.find('.box-body').first().replaceWith(data.html);
+    }
+    
+  }
+});
