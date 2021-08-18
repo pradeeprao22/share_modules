66ec085a56ffb7b799057d8d6e8e21324d0bddbb
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2021-08-18 20:21:49 +0100

    feat: visitor details city country

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,13 @@
 class PagesController < ApplicationController
   def userlanding
     ip = request.ip
-    # city = request.city
-    # country = request.country
-    VisitornewDetail.create(ip: ip)
+    @visitor_details = VisitornewDetail.create(ip: ip)
+    if request.location.city != nil
+      @visitor_details.update(city: request.location.city)
+    end
+    if request.location.country != nil
+      @visitor_details.update(country = request.location.country)
+    end
     if user_signed_in?
       redirect_to posts_path
     end
