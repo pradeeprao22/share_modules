73aae794eb6ba8f831345c7ea974ccb97ff562af
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2021-08-25 18:25:14 +0100

    fix: profile like and bookmark

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,16 +15,16 @@ class UsersController < ApplicationController
 
     def likes
         @user = User.find(params[:id])
-        @posts = @user.posts.paginate(:page => params[:page], :per_page => 9).includes(:photos, :likes, :comments)
-        @bookmarked_posts = @posts.paginate(:page => params[:page], :per_page => 9).joins(:bookmarks).where(user_id: current_user.id)
-        @liked_posts = @posts.paginate(:page => params[:page], :per_page => 9).joins(:likes).where(user_id: current_user.id)
+        @posts = @user.posts.includes(:photos, :likes, :comments)
+        @bookmarked_posts = @posts.paginate(:page => params[:page], :per_page => 6).joins(:bookmarks).where(user_id: @user.id)
+        @liked_posts = @posts.paginate(:page => params[:page], :per_page => 6).joins(:likes).where(user_id: @user.id)
     end
 
     def bookmark
         @user = User.find(params[:id])
-        @posts = @user.posts.paginate(:page => params[:page], :per_page => 9).includes(:photos, :likes, :comments)
-        @liked_posts = @posts.paginate(:page => params[:page], :per_page => 9).joins(:likes).where(user_id: current_user.id)
-        @bookmarked_posts = @posts.paginate(:page => params[:page], :per_page => 9).joins(:bookmarks).where(user_id: current_user.id)
+        @posts = @user.posts.includes(:photos, :likes, :comments)
+        @liked_posts = @posts.paginate(:page => params[:page], :per_page => 6).joins(:likes).where(user_id: @user.id)
+        @bookmarked_posts = @posts.paginate(:page => params[:page], :per_page => 6).joins(:bookmarks).where(user_id: @user.id)
     end
 
 end
