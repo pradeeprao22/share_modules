345092f0a1c20075e60e415103b7df0e19a8989c
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2021-08-26 13:39:46 +0100

    feat: add message notification

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -5,12 +5,18 @@ class NotificationsController < ApplicationController
         # Notification should comes from Init
         # Like
         # Bookmark
-
         @current_user_posts = current_user.posts
-
+        # if @current_user_posts != nil
+        #     respond_to do |format|
+        #         format.js { render 'create_notifications'} 
+        #     end
+        # end
         # Notification update through react frontend 
         
         # params user liked your post (user_post)
+        # It should be recent
+        # This users sent message to you
+        @messages_from_user = Message.all.where(user_id: current_user)
     end
 
 end
