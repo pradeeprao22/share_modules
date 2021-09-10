2a5296f703b80bf1efdd345115c27c116b91d290
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2021-09-10 17:08:37 +0100

    feat: fake post migration change

--- db/module_backend_migrate/20210907200028_create_fake_posts.rb
+++ db/module_backend_migrate/20210907200028_create_fake_posts.rb
@@ -1,7 +1,7 @@
 class CreateFakePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :fake_posts do |t|
-      t.string :content
+      t.string :discription
 
       t.timestamps
     end
