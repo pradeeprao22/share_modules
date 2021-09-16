88983b147cf112a8fe1d6ad280501a1eb1afee9e
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2021-09-16 17:31:00 +0100

    feat: post controller update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,28 @@ class PostsController < ApplicationController
   end
 
   def create
+    # request from module_post.html.erb will come here
+    # Accroding to the cases tables will be created
+    if post_params[:module_type] == "1"
+      case
+      when post_params[:database_id] == "1"
+        10.times do # number select by user
+          FakePost.create(description: Faker::Internet.name)
+        end  
+      when post_params[:database_id] == "2"
+        10.times do
+          FakeUser.create(username: Faker::Internet.username)
+        end
+      when post_params[:database_id] == "5"
+        10.times do
+          FakeCategorie.create(name: Faker::Internet.name)
+        end
+      when post_params[:database_id] == "4"
+        10.times do
+          FakeContact.create(email: Faker::Internet.email)  
+        end 
+      end
+    end
         # For getting user details
         action = params[:action]
         getdetails(action)
@@ -27,8 +49,18 @@ class PostsController < ApplicationController
             if params[:images]
                 params[:images].each do |img|
                   @post.photos.create(image: img)
-                  ActionCable.server.broadcast('post_channel', post: ( render @post))
-                  head :ok
+                  # ActionCable.server.broadcast('post_channel', post: ( render @post))
+                  if @post.module_type == "1"
+                    redirect_to new_columns_for_fake_databases_path
+                  else
+                    flash[:notice] = "Post submitted success now create columns for you backend module.."
+                  end
+                  # head :ok
+                  # PostBroadcastJob.perform_later
+                  # As soon as post get created colmuns table creation will get called
+                  # Under development
+                  # As soon as it create it will start building build
+                  # redirect_to building_path(@post.slug)
                   # PostBroadcastJob.perform_later
                 end
             end
@@ -84,8 +116,26 @@ class PostsController < ApplicationController
   end
 
   def module_post
+    # if params[:database_table_id].present?
+    #   @module_type = DatabaseTable.find(database_table_id).module_type
+    # else
+    #   @module_types = ModuleType.all
+    # end
+
+    # if request.xhr?
+    #   respond_to do |format|
+    #     format.json {
+    #       render json: {module_types: @module_types}
+    #     }
+    #   end
+    # end
     @post = Post.new
   end
+
+  #under dev backend module for execution
+  # def backend_module_execution
+  #   code_execute
+  # end
 
   def build_module
     # for getting user details
@@ -93,13 +143,22 @@ class PostsController < ApplicationController
     getdetails(action)
 
     #under dev
-    get_backend_code(@post.id)   
+    if @post.module_type == "1"
+      get_backend_code(@post.id)
+    end
 
     @photos = @post.photos
     @likes = @post.likes.includes(:user)
     @comment = Comment.new
     @is_liked = @post.is_liked(current_user)
     @is_bookmarked = @post.is_bookmarked(current_user)
+    @fake_user = FakeUser.all
+    @fake_contact = FakeContact.all
+    @fake_categorie = FakeCategorie.all
+    @fake_post = FakePost.all
+    # under dev
+    # post_id = @post.id
+    # backend_module_execution(post_id)
   end
 
   #under dev
@@ -108,11 +167,6 @@ class PostsController < ApplicationController
 
     # In Dev
     @import = Import.create(import_client: params[:import_client], user_id: current_user.id, post_id: @post.id)
-  end
-
-  #under dev
-  def backend_module_execution
-    code_execute
   end
 
   private
@@ -126,7 +180,7 @@ class PostsController < ApplicationController
   end
 
   def post_params
-    params.require(:post).permit(:content, :frontend, :javascript, :backend, :frontend_css, :instruction, :slug)
+    params.require(:post).permit(:content, :frontend, :javascript, :backend, :frontend_css, :database_id, :instruction, :slug, :module_type)
   end
 
 end
