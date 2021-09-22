f81aa704fde0c74afa7e70acd36eca8c122c8da9
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2021-09-22 15:39:31 +0100

    feat: add imgkit background worker

---
+++ app/workers/imgkit_worker.rb
@@ -0,0 +1,33 @@
+class ImgkitWorker
+  include Sidekiq::Worker
+  sidekiq_options retry: false
+
+  def perform(slug, post_id) 
+    if Rails.env == "development"
+        kit = IMGKit.new('http://localhost:3000/posts/build_module/'+ slug)
+        img = kit.to_img(:png)
+          
+        if ImageRepo.find_by(image_name: slug) == nil
+          image_file = kit.to_file('app/assets/images/module_screenshots/'+ slug+'.png')
+          ImageRepo.create(image_name: slug, post_id: post_id)
+        end
+  
+    elsif Rails.env == "production"
+        kit = IMGKit.new("http://ai.londevs.com/posts/build_module/"+ slug)
+        img = kit.to_img(:png)
+  
+        if ImageRepo.find_by(image_name: slug) == nil
+          image_file = kit.to_file('app/assets/images/module_screenshots/'+ slug+'.png')
+          ImageRepo.create(image_name: slug, post_id: post_id)
+        end
+  
+    else 
+        flash[:notice] = "Some error happened"
+    end
+  end
+end
+
+# class ImgkitRemoveWorker
+#   include Sidekiq::Worker
+#   sidekiq_options retry: false
+# end
