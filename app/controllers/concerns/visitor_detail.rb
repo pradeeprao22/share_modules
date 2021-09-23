9a092581ec4e7e30e416bf9fb07ea82e7141d38f
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2021-09-23 15:23:27 +0100

    feat: geocode large number of request

--- app/controllers/concerns/visitor_detail.rb
+++ app/controllers/concerns/visitor_detail.rb
@@ -7,8 +7,8 @@ module VisitorDetail
     # if request.location.city != nil
     #   @visitor_details.update(city: request.location.city, country: request.location.country)
     # end
-    if request.location.country != nil
-      @visitor_details.update(country: request.location.country)
-    end
+    # if request.location.country != nil
+    #   @visitor_details.update(country: request.location.country)
+    # end
    end 
 end
