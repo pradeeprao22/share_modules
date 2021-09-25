f95cb8ef7d29c9747f49c1a7fbcd9fbf935dfe0b
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2021-09-25 14:29:52 +0100

    fix: tag seed add

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,26 @@
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+if AdminUser.find_by(email: 'admin@example.com') == nil
+   AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+end
 
 if Rails.env.development?
+
+    if DatabaseTable.all.count != nil
     DatabaseTable.create!(databasename: "Londevs", databasetable: "USER")
     DatabaseTable.create!(databasename: "Londevs", databasetable: "POST")
     DatabaseTable.create!(databasename: "Londevs", databasetable: "LIKE")
     DatabaseTable.create!(databasename: "Londevs", databasetable: "CONTACTTABLE")
+    end 
 
+    if ModuleType.all.count != nil
     ModuleType.create(name: "BACKEND")
     ModuleType.create(name: "FRONTEND")
+    end
+
+    Tag.create(name: "html")
+    Tag.create(name: "CSS")
+    Tag.create(name: "Javascript")
+    Tag.create(name: "ruby")
+    Tag.create(name: "rubyonrails")
+    Tag.create(name: "python")
+    
 end
