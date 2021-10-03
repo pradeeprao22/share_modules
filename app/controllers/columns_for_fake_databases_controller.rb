37f1572554270016e9524d5ce33b28bd3bc74704
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2021-10-03 17:08:16 +0100

    feat: method logic changed ColumnsForFakeDatabase

--- app/controllers/columns_for_fake_databases_controller.rb
+++ app/controllers/columns_for_fake_databases_controller.rb
@@ -1,23 +1,23 @@
 class ColumnsForFakeDatabasesController < ApplicationController
 
+  before_action :authenticate_user!
   def create
     @column =  ColumnsForFakeDatabase.new(column_params)
     @column.user_id = current_user.id
 
-    if (@column.user_id == Post.last.user_id) && (@column.user_id == current_user)
+    if (@column.user_id == Post.last.user_id) && (@column.user_id == current_user.id)
         @column.post_id = Post.last.id
-        DatabaseAndItsColumn.create(columns_for_fake_database_id: @column.id, database_table_id: Post.last.database_id, post_id: Post.last.id)
+        @column.save!
+        @post = Post.last
+        @column.update(post_id: Post.last.id)
+        DatabaseAndItsColumn.create(columns_for_fake_database_id: @column.id, database_table_id: Post.last.database_id, post_id: Post.last.id, user_id: current_user.id)
+        flash[:notice] = "Columns for database has been created"
+       # redirect_to post_path(@post)
         #@column.database_table_id << Post.last.database_id
     else
       flash[:notice] = "Some error happened"
     end
     
-    if @column.save!
-      @post = Post.last
-      @column.update(post_id: Post.last.id)
-      redirect_to post_path(@post)
-      flash[:notice] = "Columns for database has been created"
-    end
   end
 
   def new
