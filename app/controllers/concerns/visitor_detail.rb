d64ad553c5d39b3c2950c8d46517ec2ee9478233
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2021-10-04 19:23:03 +0100

    hold visitor_detail feature

--- app/controllers/concerns/visitor_detail.rb
+++ app/controllers/concerns/visitor_detail.rb
@@ -3,7 +3,7 @@ module VisitorDetail
 
    def getdetails action
     ip = request.ip
-    @visitor_details = VisitornewDetail.create(ip: ip, page: action)
+    @visitor_details = VisitornewDetail.new(ip: ip, page: action)
     # if request.location.city != nil
     #   @visitor_details.update(city: request.location.city, country: request.location.country)
     # end
