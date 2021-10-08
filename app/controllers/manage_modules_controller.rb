af385fce5e5de3af0a7ea2ba8f6c9b61419aa5b6
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2021-10-08 13:18:52 +0100

    feat: new manage module

---
+++ app/controllers/manage_modules_controller.rb
@@ -0,0 +1,11 @@
+class ManageModulesController < ApplicationController
+    
+    # In Development
+    def manage_frontend
+        #According to the module type
+    end
+    
+    def manage_backend
+        #According to the module type
+    end
+end
