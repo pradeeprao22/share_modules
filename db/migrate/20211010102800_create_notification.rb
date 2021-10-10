0bd7735b3d6090ad0aacb484c653efd1712e2d04
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2021-10-10 11:49:29 +0100

    feat: notification table add

---
+++ db/migrate/20211010102800_create_notification.rb
@@ -0,0 +1,9 @@
+class CreateNotification < ActiveRecord::Migration[6.1]
+    def change
+      create_table :notifications do |t|
+        t.string :name
+        t.references :notificationable, polymorphic: true
+        t.timestamps
+      end
+    end
+end
