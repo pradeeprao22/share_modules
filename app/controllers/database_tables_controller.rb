b00001e67ec94f0f3bc53378a3f705c089c42302
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2021-10-16 14:12:59 +0100

    feat: database table controller added

--- app/controllers/database_tables_controller.rb
+++ app/controllers/database_tables_controller.rb
@@ -1,6 +1,35 @@
 class DatabaseTablesController < ApplicationController
+  before_action :authenticate_user!
+
     def index
       @database_table = DatabaseTable.all
       render json: @database_table
+    end
+
+    def create
+      byebug
+      @database_table = DatabaseTable.new(database_params)
+      @database_table.user_id = current_user
+      if @database_table.save!
+        flash[:notice] = "Database was created"
+      end
+    end
+
+    def new
+      @database_table = DatabaseTable.new
+    end
+
+    private
+
+    def find_column
+      @database = DatabaseTable.find(params[:id])
+  
+      return if @database
+      flash[:danger] = "Database not exist!"
+      redirect_to root_path
+    end
+  
+    def database_params
+      params.require(:database_table).permit(:databasename, :databasetable)
     end
 end
