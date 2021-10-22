c1db4061f8d721eb3fe976817fdf69c30b571278
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2021-10-22 13:30:59 +0100

    fix: change in database table association

--- app/models/columns_for_fake_database.rb
+++ app/models/columns_for_fake_database.rb
@@ -1,3 +1,4 @@
 class ColumnsForFakeDatabase < ApplicationRecord
     # this will be an array of columns
+    belongs_to :database_table, class_name: 'DatabaseTable', foreign_key: 'database_table'
 end
