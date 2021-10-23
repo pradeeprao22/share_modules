77c1a9df1d56fd86349a48e955f988b7746b647b
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2021-10-23 19:20:09 +0100

    feat: schema update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_20_162521) do
+ActiveRecord::Schema.define(version: 2021_10_22_120653) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 2021_10_20_162521) do
     t.integer "post_id"
     t.integer "incriment_number"
     t.string "database_table_id"
+    t.integer "database_table"
     t.index ["post_id"], name: "index_columns_for_fake_databases_on_post_id"
     t.index ["user_id"], name: "index_columns_for_fake_databases_on_user_id"
   end
