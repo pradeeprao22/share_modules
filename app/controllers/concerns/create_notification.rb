6fcfb44cc737195cb066af34cd31513e5e20c97c
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2022-02-21 23:15:44 +0000

    feat: new logic notifications

--- app/controllers/concerns/create_notification.rb
+++ app/controllers/concerns/create_notification.rb
@@ -1,7 +1,29 @@
 module CreateNotification
   extend ActiveSupport::Concern
     def notify(action, post)
-      created_notification = Notification.create(:notificationable => post)
+
+      # notification according to user follow
+      r = Follow.all.map do |follow|
+      
+        if follow.follower_id == current_user
+          
+        end
+      
+      end
+
+      @notification = Notification.new(:notificationable => post)
+
+      if notification.save!
+
+        ActionCable.server.broadcast 'notification',
+        notification: @message.body,
+
+        created: notification.created_at
+
+      head :ok
+
+      end
+
       created_notification.update(name: action)
     end
 end
