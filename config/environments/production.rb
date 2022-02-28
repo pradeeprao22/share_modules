50f73dd7317ce5186a3c2212ef9e50b2ba1d013f
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2022-02-28 15:53:00 +0000

    feat: action cable setup changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
+  config.action_cable.url = 'wss://ai.londevs.com/cable'
   config.action_cable.allowed_request_origins = [%r{https?://\S+}]
 
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
