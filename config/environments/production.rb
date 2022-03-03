cb2a648176839eae5733a3df17e0f38b8e5a43d6
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2022-03-03 17:34:02 +0000

    feat: env file update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,13 +43,14 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  config.action_cable.url = 'wss://ai.londevs.com/cable'
-  config.action_cable.allowed_request_origins = [%r{https?://\S+}]
+  #config.action_cable.url = 'wss://ai.londevs.com/websocket'
+  #config.action_cable.allowed_request_origins = [%r{https?://\S+}]
+  #config.action_cable.allowed_request_origins = ['http://ai.londevs.com','http://modulerrr.herokuapp.com']
 
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = false
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
