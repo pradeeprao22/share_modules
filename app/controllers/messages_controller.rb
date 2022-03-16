5dc8d5e7ff0d12bd404f031401b649c1f7b2a14c
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2022-03-16 23:55:30 +0000

    feat: messages module update

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,21 @@
 class MessagesController < ApplicationController
-    before_action do
+  include ActionController::Cookies
+  before_action :set_cookie, only: [:index]
+  
+      before_action do
         @conversation = Conversation.find_by_id(params[:conversation_id])
       end
       
+      def set_cookie
+        cookies[:verified_user] = current_user.id
+        verified_user = cookies[:verified_user]
+      end
+
       def index
+        @conversations = Conversation.all
         @messages = @conversation.messages
         @message = @conversation.messages.new
-      end
+      end 
 
       def messages_index
         @messages = @conversation.messages
@@ -20,8 +29,32 @@ class MessagesController < ApplicationController
       def create
         @message = @conversation.messages.new(conversation_id: params[:conversation_id], user_id: params[:message][:user_id], body: params[:message][:body])
         if @message.save
-          redirect_to conversation_messages_path(@conversation)
-        end
+            #message sender
+            @user_send = @message.user.id.to_i
+            @verified_user = cookies[:verified_user].to_i
+            
+            #byebug
+            if @conversation.sender.id == @verified_user.to_i
+               @user_rec = @conversation.recipient.id.to_i
+            elsif @conversation.recipient.id == @verified_user.to_i
+               @user_rec = @conversation.sender.id.to_i
+            else
+               flash[:notice] = "Some error"
+            end
+            #message sent current user
+            #@message_sent_user = @conversation.recipient.id
+
+          #byebug
+          ActionCable.server.broadcast 'message',
+            message: @message.body,
+            user_send: @user_send,
+            user_rec: @user_rec,
+            #user_rec: @user_rec,
+            verified_user: @verified_user,
+            created: @message.created_at
+          head :ok
+          # redirect_to conversation_messages_path(@conversation)
+          end
       end
       
       private
