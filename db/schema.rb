6018ffeada288519fbfd8bcd9ec1d39fcc10598e
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2022-03-16 23:38:57 +0000

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_31_202451) do
+ActiveRecord::Schema.define(version: 2022_02_22_170207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -229,6 +229,7 @@ ActiveRecord::Schema.define(version: 2021_10_31_202451) do
     t.bigint "notificationable_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
     t.index ["notificationable_type", "notificationable_id"], name: "index_notifications_on_notificationable"
   end
 
