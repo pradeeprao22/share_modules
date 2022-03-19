61eafd6a81c6b15fd1c29e60094bbd33d6b7a608
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2022-03-19 17:56:53 +0000

    feat: notification fetch according to the user

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,3 +1,7 @@
 class Notification < ApplicationRecord
     belongs_to :notificationable, polymorphic: true
+
+    def self.current_user_notifications(current_user)
+       where(user_id: current_user.id)
+    end
 end
