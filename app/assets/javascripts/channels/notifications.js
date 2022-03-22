8f1a18e205ebddd68fa95a9573e264c9786d5d30
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2022-03-22 19:37:58 +0000

    fix: console.log removed

--- app/assets/javascripts/channels/notifications.js
+++ app/assets/javascripts/channels/notifications.js
@@ -10,10 +10,9 @@ LondevApp.notifications = LondevApp.cable.subscriptions.create('NotificationsCha
     renderMessage: function(data) {
     
     //console.log(data)
-
     //var allcookies = document.cookie;
     //cookiearray = allcookies.split(';');
-    var user = getCookie('verified_user')
+    //var user = getCookie('verified_user')
 
     return "<div class='border-card'><div class='content-wrapper'><p class='title'>"+ data.message + "</p><p class='title'>" +  data.created + "</p></div><i class='material-icons end-icon'>more_vert</i></div>";
 
