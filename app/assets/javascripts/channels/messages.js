daedc350e54ae515b594d6c9767b5ace98ad8bdb
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2022-03-22 16:24:10 +0000

    feat: change in messages channel

--- app/assets/javascripts/channels/messages.js
+++ app/assets/javascripts/channels/messages.js
@@ -17,11 +17,11 @@ LondevApp.messages = LondevApp.cable.subscriptions.create('MessagesChannel', {
     
         if (data.current_user === cookie_user){
             // return "<p> <b>" + data.user + ": </b>" + data.message + "</p>";
-            return "<div class='outgoing_msg'><div class='sent_msg'><p>" + data.message + "</p><span class='time_date'>"  + data.created + "</span></div></div>";
+            return "<div class='incoming_msg'><div class='incoming_msg_img'><img class='card-type-icon with-border material-icons' src='http://www.gravatar.com/avatar/9d496db5983cc0623a2c400e334382b3.jpg'></div><div class='received_msg'><div class='received_withd_msg'><p>" + data.message + "</p><span class='time_date'>"  + data.created + "</span></div></div></div>";
             // } else if (data.user_rec === data.current_user) {  
             //  return "<div class='incoming_msg'><div class='received_msg'><div class='received_withd_msg'><p>" + data.message + "</p><span class='time_date'>"  + data.created + "</span></div></div></div>";
         } else {
-            return "<div class='incoming_msg'><div class='incoming_msg_img'><img class='card-type-icon with-border material-icons' src='http://www.gravatar.com/avatar/9d496db5983cc0623a2c400e334382b3.jpg'></div><div class='received_msg'><div class='received_withd_msg'><p>" + data.message + "</p><span class='time_date'>"  + data.created + "</span></div></div></div>";
+            return "<div class='outgoing_msg'><div class='sent_msg'><p>" + data.message + "</p><span class='time_date'>"  + data.created + "</span></div></div>";
         }
     }
 
