c5a7f735d80d0c19404bd3e9651f5a3096c980c3
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2022-04-30 15:03:00 +0100

    fix: order of js library changed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery3
 //= require_self
 //= require rails-ujs
 //= require activestorage
-//= require jquery3
 //= require popper
 //= require bootstrap-sprockets
 //= require dropzone
