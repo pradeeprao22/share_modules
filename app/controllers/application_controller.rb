107a1ac8eafcd8e60697e2f698fef20a526703ad
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2022-05-16 16:00:59 +0100

    fix: exception for the error added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
-        protect_from_forgery with: :exception
+        #protect_from_forgery with: :exception
+        rescue_from ActionController::InvalidAuthenticityToken, :with => :bad_token
 
         before_action :configure_permitted_parameters, if: :devise_controller?
+
+        def bad_token
+           flash[:warning] = "Session expired"
+           redirect_to root_path
+        end
 
         protected
         def configure_permitted_parameters
