8be3f8fa2002510f36360bc535ede32de105875d
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2022-05-30 21:49:02 +0100

    feat: notifications add to likes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,15 @@
 class LikesController < ApplicationController
+  include CreateNotification
   before_action :authenticate_user!
 
   def create
+    action = params[:action]
     @post = Post.find_by_slug(params[:post_slug])
     post_id = @post.id
     @like = current_user.likes.new(post_id: post_id, post_slug: params[:post_slug])
     if @like.save
+    # For creating notification
+    notify(action, @like)
         respond_to :js
     else
         flash[:alert] = "Something went wrong ..."
