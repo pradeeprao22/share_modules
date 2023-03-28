6427bde0c65aca724ffeb4805c7cf121bd42e51c
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2023-03-28 10:56:38 +0100

    schema changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_24_101510) do
+ActiveRecord::Schema.define(version: 2023_03_26_142433) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -216,6 +216,8 @@ ActiveRecord::Schema.define(version: 2023_03_24_101510) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
+    t.string "bio"
+    t.string "url"
   end
 
   create_table "messages", force: :cascade do |t|
