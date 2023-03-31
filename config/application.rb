457f30e2b21f3e85170a878fb253fa3ced960f9f
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2023-03-31 14:34:34 +0100

    asset fallback false add

--- config/application.rb
+++ config/application.rb
@@ -25,11 +25,14 @@ module LondevApp
     config.exceptions_app = ->(env) { ExceptionController.action(:show).call(env) }
     #path for fonts
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.assets.unknown_asset_fallback = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.active_job.queue_adapter = :sidekiq
+    # Disabling the CSRF FOR APPLICATION
+    # config.action_controller.default_protect_from_forgery = false # unless ENV["RAILS_ENV"] == "production"
   end
 end
