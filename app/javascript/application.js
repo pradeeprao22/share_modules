c8d1ae2daf0b7ff194457d2ed61184fc17f70464
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2023-05-22 16:27:12 +0100

    rails7 import maps install

---
+++ app/javascript/application.js
@@ -0,0 +1,8 @@
+import "./controllers"
+import "popper"
+import "bootstrap"
+import "jquery"; // this import first
+import "script"; // then your other imports that use `$`
+import "toastr";
+
+console.log($);
---
+++ config/importmap.rb
@@ -0,0 +1,20 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "application", preload: true
+pin "popper", to: 'popper.js', preload: true
+pin "bootstrap", to: 'bootstrap.min.js', preload: true      
+pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.7.0/dist/jquery.js"
+pin "toastr", to: "https://ga.jspm.io/npm:toastr@2.1.4/toastr.js", preload: true
+pin "codemirror", to: "https://ga.jspm.io/npm:codemirror@6.0.1/dist/index.js"
+pin "@codemirror/autocomplete", to: "https://ga.jspm.io/npm:@codemirror/autocomplete@6.7.1/dist/index.js"
+pin "@codemirror/commands", to: "https://ga.jspm.io/npm:@codemirror/commands@6.2.4/dist/index.js"
+pin "@codemirror/language", to: "https://ga.jspm.io/npm:@codemirror/language@6.7.0/dist/index.js"
+pin "@codemirror/lint", to: "https://ga.jspm.io/npm:@codemirror/lint@6.2.1/dist/index.js"
+pin "@codemirror/search", to: "https://ga.jspm.io/npm:@codemirror/search@6.4.0/dist/index.js"
+pin "@codemirror/state", to: "https://ga.jspm.io/npm:@codemirror/state@6.2.0/dist/index.js"
+pin "@codemirror/view", to: "https://ga.jspm.io/npm:@codemirror/view@6.12.0/dist/index.js"
+pin "@lezer/common", to: "https://ga.jspm.io/npm:@lezer/common@1.0.2/dist/index.js"
+pin "@lezer/highlight", to: "https://ga.jspm.io/npm:@lezer/highlight@1.1.4/dist/index.js"
+pin "crelt", to: "https://ga.jspm.io/npm:crelt@1.0.6/index.js"
+pin "style-mod", to: "https://ga.jspm.io/npm:style-mod@4.0.3/src/style-mod.js"
+pin "w3c-keyname", to: "https://ga.jspm.io/npm:w3c-keyname@2.2.7/index.js"
