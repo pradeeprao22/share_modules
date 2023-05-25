138889f444fb37abf3efd6876f9a178f1951b4ef
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2023-05-25 11:50:17 +0100

    new modules add to application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,18 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "jquery"
+import "@rails/actioncable"
+
+import Turbolinks from "turbolinks"
+Turbolinks.start()
+
+import Rails from "@rails/ujs"
+Rails.start()
+
 import "codemirror"
 import "./user_search"
 import "./active_admin"
 import "./codemirror_custom"
-import "./testing"
+// import "./channels"
 
 window.$ = window.jQuery = require("jquery");
