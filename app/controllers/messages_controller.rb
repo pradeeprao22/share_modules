2b1b331642e7a3d274fbdbbdc2a3cb5d1bd8f223
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2023-06-18 17:57:47 +0100

    action cable broadcast syntex change

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,15 +1,15 @@
 class MessagesController < ApplicationController
   include ActionController::Cookies
-  # before_action :set_cookie, only: [:index]
+  before_action :set_cookie, only: [:index]
   
       before_action do
         @conversation = Conversation.find_by_id(params[:conversation_id])
       end
       
-      # def set_cookie
-      #   cookies[:verified_user] = current_user.id
-      #   verified_user = cookies[:verified_user]
-      # end
+      def set_cookie
+        cookies[:verified_user] = current_user.id
+        verified_user = cookies[:verified_user]
+      end
 
       def index
         @conversations = Conversation.all
@@ -32,10 +32,7 @@ class MessagesController < ApplicationController
             #message sender
             @user_send = @message.user.id.to_i
             @verified_user = cookies[:verified_user].to_i
-            
-            #byebug
             if @conversation.sender.id == @verified_user.to_i
-              #byebug
                @user_rec = @conversation.recipient.id.to_i
             elsif @conversation.recipient.id == @verified_user.to_i
                @user_rec = @conversation.sender.id.to_i
@@ -45,13 +42,13 @@ class MessagesController < ApplicationController
             #message sent current user
             #@message_sent_user = @conversation.recipient.id
 
-          #byebug
-          ActionCable.server.broadcast 'message',
+          ActionCable.server.broadcast("message", {
             message: @message.body,
             current_user: @user_rec,
             #user_rec: @user_rec,
             verified_user: @verified_user,
-            created: @message.created_at
+            created: @message.created_at 
+          })
           head :ok
           # redirect_to conversation_messages_path(@conversation)
           end
