c0b84b0c50f929daeecc032f3bb5470e0bd3404f
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2023-07-13 13:49:54 +0100

    Facrotybot post add to post controller spec

--- spec/features/user_edit_spec.rb
+++ spec/features/user_edit_spec.rb
@@ -1,19 +1,23 @@
-# describe "Edit user details", :type => :feature do
+describe "Editing user details", :type => :feature do
 
-#     it "Edit user details" do
-#       user = User.create(:name => 'example', :email => 'user@example.com', :password => 'password')
-#       login_as user
-#       visit edit_user_registration_path
+    it "edits user details" do
+      user = User.create(:name => 'example', :email => 'user@example.com', :password => 'password')
+      login_as user
+    
+      visit '/edit'
+      #find_link('Edit Profile', wait: 10)
+
+      #click_link('Edit Profile')
       
-#       fill_in 'user[name]', :with => 'Example User'
-#       fill_in 'user[email]', :with => 'email@londevs.com'
-#       fill_in 'user[password]', :with => 'password'
-#       fill_in 'user[password_confirmation]', :with => 'password'
+      fill_in 'Name', :with => 'User'
+      fill_in 'user[email]', :with => 'email@londevs.com'
+      fill_in 'user[password]', :with => 'password'
+      fill_in 'user[password_confirmation]', :with => 'password'
 
-#       click_button 'Update'
+      click_button 'Update'
 
-#       expect(page).to have_http_status(200)
-#     end
+      expect(page).to have_http_status(200)
+    end
 
-# end
+end
   
