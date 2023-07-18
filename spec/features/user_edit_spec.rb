919fa26e2742e1e00e05ad7b19c7b551907d5a57
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2023-07-18 14:27:00 +0100

    Test cases update

--- spec/features/user_edit_spec.rb
+++ spec/features/user_edit_spec.rb
@@ -1,22 +1,23 @@
 describe "Editing user details", :type => :feature do
 
+    before :each do
+      @user = User.create(:name => 'example user', :email => 'user@example.com', :password => 'password')
+    end
+
     it "edits user details" do
-      # user = User.create(:name => 'example', :email => 'user@example.com', :password => 'password')
-      # login_as user
+      login_as @user
     
-      # visit '/edit'
-      # #find_link('Edit Profile', wait: 10)
-
-      # #click_link('Edit Profile')
+      visit '/edit'
+      # click_link('Edit Profile')
       
-      # fill_in 'Name', :with => 'User'
-      # fill_in 'user[email]', :with => 'email@londevs.com'
-      # fill_in 'user[password]', :with => 'password'
-      # fill_in 'user[password_confirmation]', :with => 'password'
+      fill_in 'user_name', :with => 'User'
+      fill_in 'user[email]', :with => 'email@londevs.com'
+      fill_in 'user[password]', :with => 'password'
+      fill_in 'user[password_confirmation]', :with => 'password'
 
-      # click_button 'Update'
+      click_button 'Update'
 
-      # expect(page).to have_http_status(200)
+      expect(page).to have_http_status(200)
     end
 
 end
