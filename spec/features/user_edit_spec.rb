5af3e53c55dbb0e8e0c2015dca1ecf0752116573
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2023-07-21 00:11:23 +0100

    Test case for user edit added

--- spec/features/user_edit_spec.rb
+++ spec/features/user_edit_spec.rb
@@ -1,24 +1,22 @@
 describe "Editing user details", :type => :feature do
 
-    # before :each do
-    #   @user = User.create(:name => 'example user', :email => 'user@example.com', :password => 'password')
-    # end
+    before :each do
+      @user = FactoryBot.create(:user)
+    end
 
-    # it "edits user details" do
-    #   login_as @user
-    
-    #   visit edit_user_registration_path
-    #   # click_link('Edit Profile')
+    it "edits user details" do
+      login_as @user
+      visit edit_user_registration_path
       
-    #   fill_in 'user[name]', :with => 'User'
-    #   fill_in 'user[email]', :with => 'email@londevs.com'
-    #   fill_in 'user[password]', :with => 'password'
-    #   fill_in 'user[password_confirmation]', :with => 'password'
+      fill_in 'Name', :with => 'User Example'
+      fill_in 'Email', :with => 'email@londevs.com'
+      fill_in 'Password', :with => 'password'
+      fill_in 'Password confirmation', :with => 'password'
 
-    #   click_button 'Update'
+      click_button 'Update'
 
-    #   expect(page).to have_http_status(200)
-    # end
+      expect(page).to have_http_status(200)
+    end
 
 end
   
