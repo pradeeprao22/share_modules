0796f57cf859369c3f1e5bb52838e8900ddd3398
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2023-07-22 00:31:41 +0100

    Update module test update

--- spec/features/update_module_spec.rb
+++ spec/features/update_module_spec.rb
@@ -1,24 +1,22 @@
 describe "user update the module", :type => :feature do
     
     before :each do
-      user = User.create(:name => 'Example user', :email => 'user@example.com', :password => 'password')
+      @user = FactoryBot.create(:user)
+      @post = FactoryBot.build(:post)
+      @post.user = @user
+      @post.save!
     end
   
     it "user update the module" do
-        post = Post.create(content: "Name of the content", frontend: "<html></html>", 
-            frontend_css: "<style></style>", javascript: "<script></script>", instruction: "new instructions"
-        )
+        login_as @user
+        visit posts_path(@post.slug)
 
-        visit posts_path(post.slug)
+        fill_in 'HTML', with: 'This is sample post'
+        fill_in 'CSS', with: 'This is sample post'
+        fill_in 'JAVASCRIPT', with: 'This is sample javascript'
+        fill_in 'INSTRUCTIONS', with: 'This are the instructions'
 
-        # find('post_content', visible: false).set('This is sample post')
-
-        # attach_file("UPLOAD SNAPSHOT", Rails.root + "spec/fixtures/module-screenshot.png")
-
-        # fill_in 'post[frontend]', with: 'This is sample post'
-        # fill_in 'post[frontend_css]', with: 'This is sample post'
-        # fill_in 'post[javascript]', with: 'This is sample javascript'
-        # fill_in 'post[instruction]', with: 'This are the instructions'
+        attach_file("UPLOAD SNAPSHOT", Rails.root + "spec/fixtures/module-screenshot.png")
 
         # click_button 'Update Module', disabled: true
     end
