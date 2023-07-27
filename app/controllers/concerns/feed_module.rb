db664cfdacad09c50ddb1a4d5f280c8bf8e8750d
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2023-07-27 15:50:26 +0100

    Feed module added

--- app/controllers/concerns/feed_module.rb
+++ app/controllers/concerns/feed_module.rb
@@ -1,6 +1,16 @@
 module FeedModule
     extend ActiveSupport::Concern
-    # def feed_creation feed
-    #     @feed = Feed.create(feed)
-    # end
+    def feed_creation(post_id, user_id)
+        @feed = Feed.new(post_id: post_id, user_id: user_id)
+        if @feed.save!
+            puts feed created
+        end
+    end
+
+    def fetch_feed
+        @feed = Feed.include(:post)
+        @feed.map do |feed|
+            puts "#{feed.post.user} posted this #{feed.post.content}"
+        end
+    end
 end
