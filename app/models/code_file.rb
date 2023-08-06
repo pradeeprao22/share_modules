2a9dd34bbcd47f89185a0cf4cfe6ccc2472e9aea
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2023-08-06 12:45:43 +0100

    Code files migration update

--- app/models/code_file.rb
+++ app/models/code_file.rb
@@ -1,5 +1,13 @@
 class CodeFile < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  after_create :add_fileid
+
+  def add_fileid
+    random = SecureRandom.hex(4)
+    self.fileid = random
+    self.save!
+  end
   
 end
--- db/migrate/20230802141742_create_code_files.rb
+++ db/migrate/20230802141742_create_code_files.rb
@@ -1,7 +1,11 @@
 class CreateCodeFiles < ActiveRecord::Migration[7.0]
   def change
     create_table :code_files do |t|
+      # t.string :html
+      # t.string :css
+      # t.string :javascript
       t.string :name
+      t.integer :fileid
       t.integer :size
       t.string :file_type
       t.string :post_column
