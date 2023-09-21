e2b96cae00851e2ecbd3816158e2b00e88b39448
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2023-09-21 10:04:01 +0530

    Codemirror js removed

--- app/javascript/codemirror_custom.js
+++ app/javascript/codemirror_custom.js
@@ -1,33 +1,33 @@
 if (window.location.pathname == "/post/module_post") {
 
-document.addEventListener("DOMContentLoaded", () => {
-    const htmlcode = $(".html-area")[0];
-    const editor = CodeMirror.fromTextArea(htmlcode, {
-      lineNumbers: true,
-      mode: "htmlmixed",
-      htmlMode: true,
-      tabMode: "indent",
-    });
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//     const htmlcode = $(".html-area")[0];
+//     const editor = CodeMirror.fromTextArea(htmlcode, {
+//       lineNumbers: true,
+//       mode: "htmlmixed",
+//       htmlMode: true,
+//       tabMode: "indent",
+//     });
+// });
 
-document.addEventListener("DOMContentLoaded", () => {
-    const htmlcode = $(".css-area")[0];
-    const editor = CodeMirror.fromTextArea(htmlcode, {
-      lineNumbers: true,
-      mode: "htmlmixed",
-      htmlMode: true,
-      tabMode: "indent",
-    });
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//     const htmlcode = $(".css-area")[0];
+//     const editor = CodeMirror.fromTextArea(htmlcode, {
+//       lineNumbers: true,
+//       mode: "htmlmixed",
+//       htmlMode: true,
+//       tabMode: "indent",
+//     });
+// });
 
-document.addEventListener("DOMContentLoaded", () => {
-    const htmlcode = $(".javascript-area")[0];
-    const editor = CodeMirror.fromTextArea(htmlcode, {
-      lineNumbers: true,
-      mode: "htmlmixed",
-      htmlMode: true,
-      tabMode: "indent",
-    });
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//     const htmlcode = $(".javascript-area")[0];
+//     const editor = CodeMirror.fromTextArea(htmlcode, {
+//       lineNumbers: true,
+//       mode: "htmlmixed",
+//       htmlMode: true,
+//       tabMode: "indent",
+//     });
+// });
 
 }
