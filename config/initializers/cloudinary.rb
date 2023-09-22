f7a84e761cd34964a5fc71d2296ba7bb98424331
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2023-09-22 11:37:51 +0530

    Cloudnary removed

--- config/initializers/cloudinary.rb
+++
@@ -1,6 +0,0 @@
-Cloudinary.config do |config|
-    config.cloud_name = 'drhctvmrj'
-    config.api_key = '695416946575491'
-    config.api_secret = 'wOLuqWYl_GcmwyAz9t9ywbMlX8k'
-    config.cdn_subdomain = true
- end
