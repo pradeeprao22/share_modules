12de4e14ed888393c49f1ce89356fc86bd7280e7
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2023-09-27 10:35:53 +0530

    Code restructured

--- app/controllers/concerns/code_filesource.rb
+++ app/controllers/concerns/code_filesource.rb
@@ -1,45 +1,38 @@
 module CodeFilesource
     extend ActiveSupport::Concern
-    
+
     def code_file_create(post)
-        if params[:post][:code_files]
-            params[:post][:code_files].each do |code_file|
-                name = code_file[:file].original_filename
-                file_type = code_file[:file].content_type
-                post_column = code_file[:post_column]
-                post_id = post.id
-                user_id = post.user.id
+        params.fetch(:post, {}).fetch(:code_files, []).each do |code_file|
+            name = code_file[:file].original_filename
+            file_type = code_file[:file].content_type
+            post_column = code_file[:post_column]
+            post_id = post.id
+            user_id = post.user.id
 
-                #Saving each uploaded file to a specific directory
-                File.open(Rails.root.join("app/assets/code_files/#{name}"), 'wb') do |file|
+            # Saving each uploaded file to a specific directory
+            file_path = File.join(Rails.root, "app", "assets", "code_files", name)
+            File.open(file_path, "wb") do |file|
                 file.write(code_file[:file].tempfile.read)
-                end
-
-                #Extracting the file size
-                size = File.size(Rails.root.join("app/assets/code_files/#{name}"))
-
-                # Empty array for stroing files code
-                files = []
-
-                File.foreach(Rails.root.join("app/assets/code_files/#{name}")) do |file|
-                    files << file.chomp
-
-                    f = files.join()
-                    if post_column == "html"
-                        post.frontend = f
-                    elsif post_column == "css"
-                        post.frontend_css = f
-                    elsif post_column == "javascript"
-                        post.javascript = f       
-                    end
-
-                    post.save!
-                end
-
-                post.code_files.new(name: name, size: size, file_type: file_type, post_column: post_column, post_id: post_id, user_id: user_id)
-                post.save!
             end
+
+            # Extracting the file size
+            size = File.size(file_path)
+
+            # Read the entire file into a string
+            content = File.read(file_path)
+
+            # Assign the file content to the correct post column
+            case post_column
+            when "html"
+                post.frontend = content
+            when "css"
+                post.frontend_css = content
+            when "javascript"
+                post.javascript = content
+            end
+
+            post.code_files.new(name: name, size: size, file_type: file_type, post_column: post_column, post_id: post_id, user_id: user_id)
+            post.save!
         end
     end
-
 end
